43fac94179d58a0d0301f4f373685d0f9db66669
AhzamSalik786 <ahzamsalik786@gamil.com>
2021-01-28 04:41:28 +0500

    connention with mongoDB

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,82 +1,76 @@
-const products = [
-    {
-        _id: '1',
-        name: 'Second Wife Resturant',
-        image: '/images/secondwife.jpg',
-        description:
-            'aa',
-        brand: 'Apple',
-        category: 'Electronics',
-        price:89 ,
-        countInStock: true,
-        rating: 2.5,
-        numReviews : 4,
-    },
-    {
-        _id: '2',
-        name: 'Al-Basit Resturant',
-        image: '/images/albasit.jpg',
-        description:
-            'aa',
-        brand: 'Apple',
-        category: 'Electronics',
-        price:89 ,
-        countInStock: 3,
-        rating: 4.5,
-        numReviews : 4,
-    },
-    {
-        _id: '3',
-        name: 'King Broast Resturant',
-        image: '/images/kingbroast.jpg',
-        description:
-            'aa',
-        brand: 'Apple',
-        category: 'Electronics',
-        price:89 ,
-        countInStock: 3,
-        rating: 4.5,
-        numReviews : 4,
-    },
-    {
-        _id: '4',
-        name: 'Lucky Broast Resturant',
-        image: '/images/luckybroast.jpg',
-        description:
-            'aa',
-        brand: 'Apple',
-        category: 'Electronics',
-        price:89 ,
-        countInStock: 3,
-        rating: 4.5,
-        numReviews : 4,
-    },
-    {
-        _id: '5',
-        name: 'King Broast Resturant',
-        image: '/images/kingbroast.jpg',
-        description:
-            'aa',
-        brand: 'Apple',
-        category: 'Electronics',
-        price:89 ,
-        countInStock: 3,
-        rating: 4.5,
-        numReviews : 4,
-    },
-    {
-        _id: '6',
-        name: 'Lucky Broast Resturant',
-        image: '/images/luckybroast.jpg',
-        description:
-            'aa',
-        brand: 'Apple',
-        category: 'Electronics',
-        price:89 ,
-        countInStock: 3,
-        rating: 4.5,
-        numReviews : 4,
-    }
+// const products = [
+//     {
+//         name: 'Second Wife Resturant',
+//         image: '/images/secondwife.jpg',
+//         description:
+//             'aa',
+//         brand: 'Apple',
+//         category: 'Electronics',
+//         price:89 ,
+//         countInStock: true,
+//         rating: 2.5,
+//         numReviews : 4,
+//     },
+//     {
+//         name: 'Al-Basit Resturant',
+//         image: '/images/albasit.jpg',
+//         description:
+//             'aa',
+//         brand: 'Apple',
+//         category: 'Electronics',
+//         price:89 ,
+//         countInStock: 3,
+//         rating: 4.5,
+//         numReviews : 4,
+//     },
+//     {
+//         name: 'King Broast Resturant',
+//         image: '/images/kingbroast.jpg',
+//         description:
+//             'aa',
+//         brand: 'Apple',
+//         category: 'Electronics',
+//         price:89 ,
+//         countInStock: 3,
+//         rating: 4.5,
+//         numReviews : 4,
+//     },
+//     {
+//         name: 'Lucky Broast Resturant',
+//         image: '/images/luckybroast.jpg',
+//         description:
+//             'aa',
+//         brand: 'Apple',
+//         category: 'Electronics',
+//         price:89 ,
+//         countInStock: 3,
+//         rating: 4.5,
+//         numReviews : 4,
+//     },
+//     {
+//         name: 'King Broast Resturant',
+//         image: '/images/kingbroast.jpg',
+//         description:
+//             'aa',
+//         brand: 'Apple',
+//         category: 'Electronics',
+//         price:89 ,
+//         countInStock: 3,
+//         rating: 4.5,
+//         numReviews : 4,
+//     },
+//     {
+//         name: 'Lucky Broast Resturant',
+//         image: '/images/luckybroast.jpg',
+//         description:
+//             'aa',
+//         brand: 'Apple',
+//         category: 'Electronics',
+//         price:89 ,
+//         countInStock: 3,
+//         rating: 4.5,
+//         numReviews : 4,
+//     }
 
-]
-export default  products;
+// ]
+// export default  products;
